2e43b6aa067c5515abc37acc20110ff7b9d0ca89
NYikolay <kolyangrans@mail.ru>
2023-02-23 20:10:58 +0300

    added STATICFILES_STORAGE as ManifestStaticFilesStorage in settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -110,11 +110,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
