b3a96ece749837df5f81c41a718e4edce73ae3b9
NYikolay <kolyangrans@mail.ru>
2023-06-27 20:51:23 +0300

    fixed generating user products data serivce.

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -9,8 +9,8 @@ CSRF_TRUSTED_ORIGINS = ['https://720e-80-249-91-162.ngrok-free.app']
 
 INTERNAL_IPS = ['127.0.0.1']
 
-# INSTALLED_APPS = INSTALLED_APPS + ['debug_toolbar']
-# MIDDLEWARE = MIDDLEWARE + ['debug_toolbar.middleware.DebugToolbarMiddleware']
-#
-# ip = socket.gethostbyname(socket.gethostname())
-# INTERNAL_IPS += [ip[:-1] + '1']
+INSTALLED_APPS = INSTALLED_APPS + ['debug_toolbar']
+MIDDLEWARE = MIDDLEWARE + ['debug_toolbar.middleware.DebugToolbarMiddleware']
+
+ip = socket.gethostbyname(socket.gethostname())
+INTERNAL_IPS += [ip[:-1] + '1']
