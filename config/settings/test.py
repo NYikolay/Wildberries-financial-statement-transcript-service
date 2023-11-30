9ebd2dcae67e949b2247adc1c603e3c99b493439
NYikolay <kolyangrans@mail.ru>
2023-11-30 21:13:42 +0300

    added web_asgi to allowed hosts

--- config/settings/test.py
+++ config/settings/test.py
@@ -5,10 +5,10 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '45.146.165.193',
     'web',
+    'web_asgi',
     'test.commery.ru'
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-
