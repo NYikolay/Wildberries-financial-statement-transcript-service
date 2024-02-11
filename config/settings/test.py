a3ca3442b6d0c81cff9bf6d562d7acc9d7be74fa
Nikolay <72572824+NYikolay@users.noreply.github.com>
2024-02-11 16:49:56 +0300

    Deleted ips and domains from allowed hosts in test settings

--- config/settings/test.py
+++ config/settings/test.py
@@ -2,12 +2,7 @@ from config.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '45.146.165.193',
-    'web',
-    'webasgi',
-    'test.commery.ru'
-]
+ALLOWED_HOSTS = []
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
