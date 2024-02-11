66b05e25cdf67e5e182c1802586c222fb313855d
Nikolay <72572824+NYikolay@users.noreply.github.com>
2024-02-11 16:48:35 +0300

    Deleted ips and domains from allowed hosts

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,11 +3,7 @@ from config.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'app.commery.ru',
-    '62.217.182.252',
-    'web'
-]
+ALLOWED_HOSTS = []
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
